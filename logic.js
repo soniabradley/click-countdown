34e04208c77b2a0feeb74a42dd2ff6b521abac0e
soniab <sonia.bradley524@gmail.com>
2018-01-14 15:23:23 -0600

    Firebase connection has failed

---
+++ logic.js
@@ -0,0 +1,25 @@
+var config = {
+    apiKey: "AIzaSyCyG6iVQ4JAEfvWYfJUMx2aSaf2-j0EkTw",
+    authDomain: "clickscountdown-5228b.firebaseapp.com",
+    databaseURL: "https://clickscountdown-5228b.firebaseio.com",
+    projectId: "clickscountdown-5228b",
+    storageBucket: "clickscountdown-5228b.appspot.com",
+    messagingSenderId: "1007305474274"
+  };
+  firebase.initializeApp(config);
+
+  var count = 100;
+
+//   A listner/ snapshot.val() is like a response from an  AJAX request but encoded
+firebase.database().ref().on("value",function(snapshot){
+    // console.log(snapshot.val());
+    count = snapshot.val().clicks;
+    $('#clickValue').html(count);
+})
+
+$('#clickButton').on("click",function(){
+    count--;
+    firebase.database().ref().set({
+        clicks:count
+    });
+})
